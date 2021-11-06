584f58a78d3d0c9ad2cf93197c6cb26d657c2e36
carlos.henrique <carlos.henrique@DSK-F2XZ3W2>
2021-11-06 11:31:23 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD/BD/Conexao.cs
@@ -0,0 +1,53 @@
+﻿using CRUD.BD.Postgree;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CRUD.BD
+{
+    public static class Conexao
+    {
+        private static IConexao conn;
+        public static void Con(IConexao con)
+        {
+            conn = con;
+            conn.Conecta();
+        }
+
+        public static void Desc(IConexao con)
+        {
+            con.Desonecta();
+        }
+
+        public static void InserirAluno(Aluno aluno, ObservableCollection<Aluno> lista)
+        {
+            conn.InserirAluno(aluno, lista);
+        }
+
+        //public static void InserirAluno(Aluno alunoPreenchido)
+        //{
+        //    InserirAluno(alunoPreenchido);
+        //}
+
+        //public static void ExcluirAluno(Aluno alunoSelecionado)
+        //{
+        //    ExcluirAluno(alunoSelecionado);
+        //}
+
+        //public static void AtualizaAluno(Aluno alunoSelecionado)
+        //{
+        //    AtualizaAluno(alunoSelecionado);
+        //}
+
+        //public static void BuscaAluno(Aluno alunoPreenchido)
+        //{
+        //    BuscaAluno(alunoPreenchido);
+        //}
+
+        //public static void BuscaTodosAlunos(Aluno alunoPreenchido)
+        //{
+        //    BuscaTodosAlunos(alunoPreenchido);
+        //}
+    }
+}
---
+++ CRUD/Utils/BaseNotify.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CRUD
+{
+    public class BaseNotify : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void Notifica(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
